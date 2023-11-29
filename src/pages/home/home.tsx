30c3845b26bb0e908b7342b6dc03d6e0983794ac
thu <nnmthu08@gmail.com>
2023-11-29 22:10:17 +0700

    t

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,10 +3,10 @@ import axios from 'axios';
 import { Slider } from './slider/slider';
 import * as S from "./style";
 import {ListCard} from "src/components/list-card";
-import { getAllFilm, getAllRap } from 'src/services/list-film';
+import { getAllFilm } from 'src/services/list-film';
 import { IIFE } from 'src/utils/homedetail';
 import { convertFilm } from './convert';
-import ListSystem from './system/list-system';
+// import ListSystem from './system/list-system';
 import {TheaterMenu} from 'src/components/TheaterMenu';
 
 function Home() {
@@ -19,13 +19,13 @@ function Home() {
   },[]);
 
 
-  const [listRap, setListRap] = useState([]);
-  useEffect(() =>{
-   IIFE(async () => {
-    const resp = await getAllRap();
-    setListRap(resp);
-   });
-  },[]);
+  // const [listRap, setListRap] = useState([]);
+  // useEffect(() =>{
+  //  IIFE(async () => {
+  //   const resp = await getAllRap();
+  //   setListRap(resp);
+  //  });
+  // },[]);
 
 
   return (
